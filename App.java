679056fd35bde95d3d9cd3244f8d4dd38e96d4f9
Dhellskiee <felicianomahilum@gmail.com>
2026-01-08 22:26:13 +0800

    Update App.java

--- App.java
+++ App.java
@@ -1,33 +1,21 @@
 public class App {
     public static void main(String[] args) {
-
         int array[] = {1, 0, 9, 4, 2, 8, 7, 3, 4};
-
-        System.out.print("Before: "); 
-        for (int i : array) {
-            System.out.print(i + ", ");
-        }
-
         quicksort(array,0, array.length - 1);
-
         System.out.print("\nAfter: ");
         for (int i : array) {
             System.out.print(i + ", ");
         }
     }
-
     public static void quicksort(int array[], int start, int end) {
         if (start >= end) return; 
-       
         int pivot = hati(array, start, end);
         quicksort(array, start, pivot - 1);
         quicksort(array, pivot + 1, end);
     }
-
     public static int hati(int array[], int start, int end) {
         int pivot = array[end];
         int i = start - 1;
-
         for (int j = start; j < end; j++) { 
             if (array[j] < pivot) {
                 i++;
@@ -36,12 +24,11 @@ public class App {
                 array[j] = temp;
             }
         }
-
         i++;
         int temp = array[i];
         array[i] = array[end];
         array[end] = temp;
-   
         return i;
     }
 }
+
